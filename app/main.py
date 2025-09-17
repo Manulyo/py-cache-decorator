968dc82704e4a25e810b263dc6e780589504031f
Alex <alex@Alex.local>
2025-09-17 22:47:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,17 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cash_store = {}
+
+    def wrapper(*args, **kwargs) -> None:
+        key = (args, tuple(sorted(kwargs.items())))
+
+        if key in cash_store:
+            print("Getting from cache")
+            return cash_store[key]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cash_store[key] = result
+        return result
+    return wrapper
